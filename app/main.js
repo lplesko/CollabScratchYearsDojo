f9bf193e21e30de1bf43cf75dd44739be82f76bc
lplesko <57364521+lplesko@users.noreply.github.com>
2020-05-28 18:10:55 -0400

    Update main.js

--- app/main.js
+++ app/main.js
@@ -93,7 +93,7 @@ define(["require", "exports", "esri/Map", "esri/views/MapView", "esri/layers/Fea
                             query = layerView.layer.createQuery();
                             query.outStatistics = [
                                 new StatisticDefinition({
-                                    onStatisticField: "Total_visits",
+                                    onStatisticField: "UniqueIndividuals_cnt",
                                     outStatisticFieldName: "value",
                                     statisticType: "sum"
                                 })
@@ -128,7 +128,7 @@ define(["require", "exports", "esri/Map", "esri/views/MapView", "esri/layers/Fea
                             query = layer.createQuery();
                             query.outStatistics = [
                                 new StatisticDefinition({
-                                    onStatisticField: "Total_visits",
+                                    onStatisticField: "UniqueIndividuals_cnt",
                                     outStatisticFieldName: "value",
                                     statisticType: "sum"
                                 })
