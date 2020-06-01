4880524037d402e761b4ce106686ca51ec939fc0
lplesko <57364521+lplesko@users.noreply.github.com>
2020-06-01 08:50:39 -0400

    Update constants.js

--- app/constants.js
+++ app/constants.js
@@ -2,5 +2,6 @@ define(["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     exports.years = ["2016", "2017", "2018", "2019"];
+    exports.blankcols = [" "];
 });
 //# sourceMappingURL=constants.js.map
