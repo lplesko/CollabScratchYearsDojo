b3a417805cff1a0e2cea104a4a4c50bd3ac2c78a
lplesko <57364521+lplesko@users.noreply.github.com>
2020-06-01 08:40:53 -0400

    Update heatmapChart.js

--- app/heatmapChart.js
+++ app/heatmapChart.js
@@ -98,10 +98,9 @@ define(["require", "exports", "esri/views/layers/support/FeatureFilter", "esri/C
     //   highlighted = null;
     // }
     function onCellSelect(cell) {
-        var year = constants_1.years[cell.col];
-        var month = constants_1.months[cell.row];
+        var year = constants_1.years[cell.row];
         if (mousemoveEnabled) {
-            highlighted = { col: cell.col, row: cell.row };
+            highlighted = { row: cell.row };
             layerView.filter = new FeatureFilter({
                 where: "YearString = '" + year + "'"
             });
