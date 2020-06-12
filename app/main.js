a7dcca8ba30e4a1ddef8476209afe40e652b481c
lplesko <57364521+lplesko@users.noreply.github.com>
2020-06-11 23:01:17 -0400

    Update main.js

--- app/main.js
+++ app/main.js
@@ -418,7 +418,8 @@ define(["require", "exports", "esri/Map", "esri/views/MapView", "esri/layers/Fea
                                     caption: "",
                                     value: {
                                       fields: ["Canada_Child_Benefit", "Disability_Benefits", "Employment", "Employment_Insurance", "No_Income", "Pension", "Provincial_Disability", "Social_Assistance", "Student_Loan", "Unknown_Income"],
-                                      normalizeField: null
+                                      normalizeField: null,
+                                      tooltopField: null
                                     }
                                   }
                                 ]
