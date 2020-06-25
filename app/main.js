4b2ed021fdc26b6ed0e6ae10d5f9e4f8dcf58a47
lplesko <57364521+lplesko@users.noreply.github.com>
2020-06-25 14:04:02 -0400

    Update main.js

--- app/main.js
+++ app/main.js
@@ -456,11 +456,6 @@ define(["require", "exports", "esri/Map", "esri/views/MapView", "esri/layers/Fea
                             color: "#00AEC7",
                             haloOpacity: 1,
                             fillOpacity: 0
-                        },
-                        popup: {
-                            visibleElements: {
-                                featureNavigation: false
-                            }
                         }
                     });
                     
